d5e395cc7ca5286161a3d58bf9c5b6e0f6965034
apo youssef <dr.apoyoussef@gmail.com>
2025-12-22 09:13:44 +0200

    add navigation for home.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -11,11 +11,13 @@ import { ConfirmationComponent } from './components/subscription-plans/confirmat
 import { PricingPageComponent } from './components/subscription-plans/pricing-page/pricing-page';
 import { ReviewsPageComponent } from './components/reviews-management/reviews-page/reviews-page';
 import { PlumberList } from "./components/plumber-list/plumber-list";
+import { Search } from "./components/search/search";
+import { Home } from "./components/home/home";
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Footer, Navbar, BasicInfoComponent, SearchResultsComponent, ReportFormComponent, ConfirmationComponent, PricingPageComponent, ReviewsPageComponent, PlumberList],
+  imports: [RouterOutlet, Footer, Navbar, BasicInfoComponent, SearchResultsComponent, ReportFormComponent, ConfirmationComponent, PricingPageComponent, ReviewsPageComponent, PlumberList, Search, Home],
 
   templateUrl: './app.html',
   styleUrl: './app.css'
